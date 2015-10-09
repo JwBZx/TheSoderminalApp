12ffc26e1ba905df30118708c32068c0e69366cb
Jonathan Wilson Bruce <jwbmw3@gmail.com>
2015-10-09 17:34:59 -0400

    Putting TheSoderminalApp Folder in GitHub :)

---
+++ TheSoderminalApp/main.js
@@ -0,0 +1,50 @@
+
+console.log("Hello! Welcome to the Soderminal App, the Official App of Soderminal!");
+console.log("In this app, you will find Information, News, and more!");
+console.log("Thank You for checking out this app, and I hope you have a good day! Thank You and God Bless! ---Jonathan Bruce");
+
+var SodaInformation = false;
+var SodaWSMCInfo = false;
+var SodaStuffWeveMade = false;
+
+prompt("Welcome! Do you want to hear information about Soderminal?");
+if(prompt = "Yes") {
+	SodaInformation = true;
+	console.log("Cool! Alright then, it's information time!");
+} else {
+	SodaInformation = false;
+	console.log("Alright, let's move on then!");
+}
+if(SodaInformation = true) {
+	console.log("Alright, it's information time!");
+	console.log("Soderminal is The World's Greatest Christian Apps, Games, Software, and Web company!");
+	console.log("We started in 2012 under the name 'SodaMan, Co.', but in 2014 we changed our name to 'Soderminal' (which is a combination of the words 'Soda' and 'Terminal').");
+	console.log("We currently have not made any Apps, Games, or Software, but there are definitely some on the way, so stay tuned for that!");
+	console.log("You can find our website online at Soderminal.Wordpress.com, or just search 'Soderminal' on your choice search engine!");
+	console.log("Thank You for reading this information! Now on to more of this app!");
+} else {
+	SodaInformation = false;
+}
+
+prompt("Do you want Social Media, Website, and Contact Information?");
+if(prompt = "Yes") {
+	SodaWSMCInfo = true;
+	console.log("Alright sweet! Here is our Website, Social Media, and Contact Information:");
+	console.log("Website:");
+	console.log("Contact Email: soderminal@gmail.com");
+	console.log("Facebook:");
+	console.log("Twitter:");
+	console.log("Google Plus:");
+	console.log("Thank You for reading through all of this Website, Social Media, and Contact Information! Now on to more of the app!");
+} else {
+	SodaWSMCInfo = false;
+}
+prompt("Do you want to hear about all of our Apps, Games, Software, and Websites?");
+if(prompt = "Yes") {
+	SodaStuffWeveMade = true;
+	console.log("Alright sweet! Here is a list of all of the stuff we have made:");
+	console.log("Well...there's nothing here...that's because we haven't released anything yet! We've worked on websites before, but have never released any apps, games, or software!");
+	console.log("Thank You for reading through this list! Now on to more of the app!");
+} else {
+	SodaStuffWeveMade = false;
+}
